056d91f651594816091c2c387610f55fb87c17cc
caterina leonelli <caterina.leonelli@gmail.com>
2022-08-26 16:50:12 +0200

    added argparse

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 #!python3
 import sys
-import params
+from params import weights_path, program
 from predict import predict_func
-from train import train
+from train import train, load_model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def main():
-    if len(sys.argv) != 4:
-        print ("""Arguements error \n
-            Usage:  python3 segm_unet.py dataset_path number_of_epochs comment \n""")
-        sys.exit(0)
+
+def train_and_predict():
+    # if len(sys.argv) != 4:
+    #     print ("""Arguments error \n
+    #         Usage:  python3 main.py dataset_path number_of_epochs comment \n""")
+    #     sys.exit(0)
     
     train()
     predict_func()
 
-
-
+def predict():
+    # if len(sys.argv) != 3:
+    #     print ("""Arguments error \n
+    #     Usage:  python3 main.py weights_path comment \n""")
+    # sys.exit(0)
+    load_model(weights_path)
+    predict_func()
 
 if __name__ == "__main__":
-    main()
+    if program=='ONLY_PREDICT':
+        predict()
+    if program=='TRAIN_AND_PREDICT':
+        train_and_predict()
+        
--- params.py
+++ params.py
@@ -1,15 +1,119 @@
 #define constants
 import sys
 import datetime
+import os
 
-description=sys.argv[3]
-base_dir=sys.argv[1]
-model_dir=base_dir+"model"
-train_dir=base_dir+"train"
-test_dir=base_dir+"test"
-MODEL_NAME="UNET"
+import argparse
+#PROG: TRAIN AND PREDICT
+args=sys.argv[1:]
+if args and args[0].startswith("TP"):
+        parser = argparse.ArgumentParser(prog='TP', usage='%(prog)s [options]')
+        parser.add_argument("-TP","--train-and-predict",
+                            help="choose train and predict program", 
+                            action="store_true", 
+                            default=False
+                    )
+        parser.add_argument("-P","--only-predict",
+                            help="choose only predict program", 
+                            action="store_true", 
+                            default=False
+                    )
+        parser.add_argument("-v","--version", 
+                     action='version', 
+                     version='%(prog)s 1.0'
+                    )
+        parser.add_argument("-e","--epochs", 
+                            nargs='+',
+                            required=False,
+                            help="number of epochs to train a model",
+                            type=int,
+                            default=3,
+                            )
+        parser.add_argument("-c","--comment", 
+                            nargs='?',
+                            required=False,
+                            help="general comment printed to log file",
+                            default="test raw",
+                            )
+        parser.add_argument("-d","--dir-dataset", 
+                            nargs='+',
+                            required=True,
+                            help="absolute directory path where dataset is stored",
+                            )
+        parser.add_argument("-m","--model-name", 
+                            nargs='+',
+                            required=True,
+                            choices=['UNET', 'TRANSFER_LEARNING_VGG16', 'TRANSFER_LEARNING_VGG19'],
+                            help="name of the model to train",
+                            )
+else:
+        parser = argparse.ArgumentParser(prog='P', usage='%(prog)s [options]')
+        parser.add_argument("-TP","--train-and-predict",
+                            help="choose train and predict program", 
+                            action="store_true", 
+                            default=False
+                    )
+        parser.add_argument("-P","--only-predict",
+                            help="choose only predict program", 
+                            action="store_true", 
+                            default=False
+                    )
+        parser.add_argument("-v","--version", 
+                            action='version', 
+                            version='%(prog)s 1.0'
+                            )
+        parser.add_argument("-mw", "--model-weights", 
+                            nargs='+',
+                            required=True,
+                            type=argparse.FileType('r'),
+                            help="absolute file path where weights of a model are saved",
+                            )
+        parser.add_argument("-c","--comment", 
+                            nargs='?',
+                            required=False,
+                            help="general comment printed to log file",
+                            default="test raw",
+                            )
+        parser.add_argument("-m","--model-name", 
+                            nargs='+',
+                            required=True,
+                            choices=['UNET', 'TRANSFER_LEARNING_VGG16', 'TRANSFER_LEARNING_VGG19'],
+                            help="name of the model to load",
+                            )
+        parser.add_argument("-d","--dir-dataset", 
+                            nargs='+',
+                            required=True,
+                            help="absolute directory path where dataset is stored",
+                            )
+args = parser.parse_args(args=args)
+
+#TODO: inizializzare meglio
+description=''
+base_dir=''
+weights_path=''
+MODEL_NAME=''
 IMAGE_SIZE = (224, 224)
 NUM_CLASSES = 3
 BATCH_SIZE = 8
-EPOCHS=int(sys.argv[2])
+EPOCHS=0
 pred_dir = base_dir+"predictions"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+if args.only_predict:
+    program='ONLY_PREDICT'
+    description=args.comment[0]
+    base_dir=args.dir_dataset[0]
+    weights_path=args.model_weights[0].name
+    MODEL_NAME=args.model_name[0]
+if args.train_and_predict:
+    program='TRAIN_AND_PREDICT'
+    description=args.comment[0]
+    base_dir=args.dir_dataset[0]
+    weights_path=args.model_weights[0].name
+    MODEL_NAME=args.model_name[0]
+    EPOCHS=args.epochs[0]
+    
+model_dir=base_dir+"model"
+train_dir=base_dir+"train"
+test_dir=base_dir+"test"
+
+  
