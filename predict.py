611dc5bec36af4ed8e715e8f133586e658384de1
caterina leonelli <caterina.leonelli@gmail.com>
2022-08-31 18:07:05 +0200

    working on metrics

--- predict.py
+++ predict.py
@@ -58,76 +58,56 @@ def predict_func():
 
 ##EVALUATE PREDICTIONS #####################################################################
     history_dict = json.load(open(model_dir + "/history.json", "r"))
-    # # Mean IOU graph
-    # graph(history_dict,
-    #       title='Mean IoU Graph',
-    #       xlabel='Epochs',
-    #       ylabel='Mean IoU',
-    #       history_name='mean_iou',
-    #       history_val_name='val_mean_iou',
-    #       save_path=pred_dir+'/mean_iou_graph.png')
-    # # Accuracy graph
-    # graph(history_dict,
-    #       title='Accuracy Graph',
-    #       xlabel='Epochs',
-    #       ylabel='Accuracy',
-    #       history_name='accuracy',
-    #       history_val_name='val_accuracy',
-    #       save_path=pred_dir+'/accuracy_graph.png')
-    # # F1 Score graph per label
-    # for label in range(0, NUM_CLASSES):
-    #     graph(
-    #         history_dict,
-    #         title="F1 Score Graph For Label "+str(label),
-    #         xlabel="Epochs",
-    #         ylabel="F1 Score",
-    #         history_name="f1score"+str(label),
-    #         history_val_name="val_f1score"+str(label),
-    #         save_path=pred_dir + "/f1_score_"+str(label)+"_graph.png",
-    #     )
+    # Mean IOU graph
+    graph(history_dict,
+          title='Mean IoU Graph',
+          xlabel='Epochs',
+          ylabel='Mean IoU',
+          history_name='mean_iou',
+          history_val_name='val_mean_iou',
+          save_path=pred_dir+'/mean_iou_graph.png')
+    # Accuracy graph
+    graph(history_dict,
+          title='Accuracy Graph',
+          xlabel='Epochs',
+          ylabel='Accuracy',
+          history_name='accuracy',
+          history_val_name='val_accuracy',
+          save_path=pred_dir+'/accuracy_graph.png')
+    # F1 Score graph per label
+    for label in range(0, NUM_CLASSES):
+        graph(
+            history_dict,
+            title="F1 Score Graph For Label "+str(label),
+            xlabel="Epochs",
+            ylabel="F1 Score",
+            history_name="f1score"+str(label),
+            history_val_name="val_f1score"+str(label),
+            save_path=pred_dir + "/f1_score_"+str(label)+"_graph.png",
+        )
 
     
     ##WEIGHTED AVERAGE F1 SCORE################################################################
+
+
     #TODO: precision and recall for predictions
     #TODO: f1 for predictions
     #TODO: f1 average for predictions
-    # from utils.metrics.f1_score import f1score_weighted_average
-    # uniques, counts = np.unique(train_masks[0].flatten(), return_counts=True)
-    # percentages = dict(zip(uniques, counts * 100 /
-    #                    len(train_masks[0].flatten())))
-    # print('*********************+')
-    # print(percentages)
-    # f1score_weighted_average(history_dict, percentages)
+    from utils.metrics.f1_score import f1score_weighted_average
+    from tensorflow.keras.utils import to_categorical
+    from sklearn.utils import compute_class_weight
+        # minuto 20 di https://www.youtube.com/watch?v=F365vQ8EndQ
+    test_masks_cat = to_categorical(test_masks, num_classes=NUM_CLASSES)
+    test_masks_cat = test_masks_cat.reshape(
+        (test_masks.shape[0], test_masks.shape[1], test_masks.shape[2], NUM_CLASSES)
+    )
+
+    # IMBALANCED CLASSIFICATION - CLASS WEIGHTS
+    class_weights = compute_class_weight(
+        "balanced",
+        classes=np.unique(test_masks.flatten()),
+        y=np.ravel(test_masks, order="C"),
+    )
+    f1score_weighted_average(history_dict, class_weights=class_weights)
 
 
-    #ROC CURVE########################################à
-
-    from sklearn.metrics import roc_curve, auc
-    from sklearn.preprocessing import label_binarize
-    from sklearn.multiclass import OneVsRestClassifier
-    from sklearn.model_selection import train_test_split
-
-    # Import some data to play with
-    train_images, train_masks = get_train_dataset()
-    X_train = train_images
-    y_train = train_masks
-    # y_train = np.expand_dims(y_train, axis=3) #May not be necessary.. leftover from previous code 
-
-    # print(y_train)
-    # print((y_train.shape))
-    # Binarize the output
-    y = label_binarize(y_train, classes=[0, 1, 2])
-    n_classes = y_train.shape[1]
-    print(n_classes)
-
-    # Learn to predict each class against the other
-    classifier = OneVsRestClassifier(model)
-    y_score = classifier.fit(X_train, y).decision_function(X_train)
-
-    # Compute ROC curve and ROC area for each class
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(NUM_CLASSES):
-        fpr[i], tpr[i], _ = roc_curve(y[:, i], y_score[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
