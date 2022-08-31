1098072b60146ff41c9fd7cc8a2142f9500cfe03
caterina leonelli <caterina.leonelli@gmail.com>
2022-08-31 18:08:05 +0200

    clean

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
 import os
 from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
 import cv2
+import tensorflow as tf
 
-from params import IMAGE_SIZE, train_dir, test_dir
+from params import BATCH_SIZE, IMAGE_SIZE, train_dir, test_dir, SIZE_X, SIZE_Y
 
 
 def get_path_arrays(dir_path, type):
@@ -66,31 +66,8 @@ def get_train_dataset():
     """Returns tuple (input, target)"""
     train_input_img_paths = get_path_arrays(train_dir, "input")
     train_mask_img_paths = get_path_arrays(train_dir, "mask")
-    # random.Random(1337).shuffle(train_input_img_paths)
-    # random.Random(1337).shuffle(train_mask_img_paths)
-    # print("Number of input samples:", len(train_input_img_paths))
-    # print("Number of mask samples:", len(train_mask_img_paths))
 
     (images,masks)= load_data_cv2(IMAGE_SIZE, train_input_img_paths, train_mask_img_paths)
-    ###############################################
-    # Encode labels... but multi dim array so need to flatten, encode and reshape
-    # from sklearn.preprocessing import LabelEncoder
-    # labelencoder = LabelEncoder()
-    # n, h, w = masks.shape
-    # masks_reshaped = masks.reshape(-1,1)
-    # masks_reshaped_encoded = labelencoder.fit_transform(np.ravel(masks_reshaped))
-    # masks_encoded_original_shape = masks_reshaped_encoded.reshape(n, h, w)
-    # np.unique(masks_encoded_original_shape)
-    # masks_input = np.expand_dims(masks_encoded_original_shape, axis=3)
-
-    #################################################
-    # train_images = np.expand_dims(train_images, axis=3)
-    # train_images = normalize(train_images, axis=1)    print(type(train_images))
-
-    ######################TEST
-    # print(type(train_mask))#numpy.ndarray
-    # print(train_images.shape) #(60, 224, 224, 3)
-    # print(train_mask.shape) #(60, 224, 224, 1)
 
     masks_input = np.expand_dims(masks, axis=3)
     return images, masks_input
@@ -98,48 +75,15 @@ def get_train_dataset():
 
 def get_test_dataset():
     """Returns tuple (input, target)"""
-    # random.Random(1337).shuffle(test_input_img_paths)
-    # random.Random(1337).shuffle(test_mask_img_paths)
+
     test_input_img_paths = get_path_arrays(test_dir, "input")
-    test_mask_img_paths = get_path_arrays(test_dir, "mask")  # è ovviamente vuoto
-    # print("Number of input samples:", len(test_input_img_paths))
-    # print("Number of mask samples:", len(test_mask_img_paths))
+    test_mask_img_paths = get_path_arrays(test_dir, "mask")  # obv empty
 
     (images, masks) = load_data_cv2(
         IMAGE_SIZE, test_input_img_paths, test_mask_img_paths
     )
-    ###############################################
-    # Encode labels... but multi dim array so need to flatten, encode and reshape
-    # from sklearn.preprocessing import LabelEncoder
-    # labelencoder = LabelEncoder()
-    # n, h, w = masks.shape
-    # masks_reshaped = masks.reshape(-1,1)
-    # masks_reshaped_encoded = labelencoder.fit_transform(masks_reshaped)
-    # masks_encoded_original_shape = masks_reshaped_encoded.reshape(n, h, w)
-
-    # np.unique(masks_encoded_original_shape)
-    # masks_input = np.expand_dims(masks_encoded_original_shape, axis=3)
-
-    #################################################
-    # train_images = normalize(train_images, axis=1)
-
     masks_input = np.expand_dims(masks, axis=3)
     return images, masks_input
-
-
-# print allinamento immagine | maschera
-# for input_path, mask_path in zip(input_img_paths, mask_img_paths):
-#     print(input_path, "|", mask_path)
-
-# #explore dataset: plot images (image, mask) vertically
-# img_in = mpimg.imread(train_input_img_paths[3])
-# img_m = mpimg.imread(train_mask_img_paths[3])
-# plt.subplot(2,1,1)
-# plt.imshow(img_in)
-# plt.subplot(2,1,2)
-# plt.imshow(img_m)
-# plt.show()
-
 
 # TODO: inizializzare meglio test_input_img_paths e test_mask_img_paths
 test_input_img_paths = get_path_arrays(test_dir, "input")
@@ -152,8 +96,9 @@ test_mask_img_paths = get_path_arrays(test_dir, "mask")  # è ovviamente vuoto
 def load_data_cv2(IMAGE_SIZE, input_img_paths, mask_img_paths):
     images = []
     for img_path in input_img_paths:
-        img = cv2.imread(img_path, 1)
-        img = cv2.resize(img, IMAGE_SIZE)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (SIZE_Y, SIZE_X))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         images.append(img)
 
     # Convert list to array for machine learning processing
@@ -164,10 +109,11 @@ def load_data_cv2(IMAGE_SIZE, input_img_paths, mask_img_paths):
     for mask_path in mask_img_paths:
         mask = cv2.imread(mask_path, 0)
         mask = cv2.resize(
-            mask, IMAGE_SIZE, interpolation=cv2.INTER_NEAREST
+            mask, (SIZE_Y, SIZE_X), #interpolation=cv2.INTER_NEAREST
         )  # Otherwise ground truth changes due to interpolation
         masks.append(mask)
 
     # Convert list to array for machine learning processing
     masks = np.array(masks)
     return images, masks
+
