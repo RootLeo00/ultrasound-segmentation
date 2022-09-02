b4c2dd3bced3ea0bfab7224c3306efe3d6063bd3
caterina leonelli <caterina.leonelli@gmail.com>
2022-09-02 21:44:57 +0200

    ADD loss, precision, recall graph ADD tikzplotlib to save images in .tex format

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tikzplotlib
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
@@ -15,20 +16,21 @@ from utils.load_dataset import (get_test_dataset, test_input_img_paths,
                                 test_mask_img_paths)
 from utils.metrics.graph import graph
 
-pred_params=dict()
+pred_params = dict()
 
 # PREDICTION
+
+
 def predict_func():
-    
+
     ##GENERATE PREDICTIONS###############################################################
-    if(not os.path.isdir(pred_dir)):
+    if (not os.path.isdir(pred_dir)):
         os.mkdir(pred_dir)
     (test_images, test_masks) = get_test_dataset()
     print("\n\n--------------PREDICT--------------------------")
-    pred_params['shape_test_imgs']=(test_images.shape)
-    pred_params['shape_test_masks']=(test_masks.shape)
-    
-    
+    pred_params['shape_test_imgs'] = (test_images.shape)
+    pred_params['shape_test_masks'] = (test_masks.shape)
+
     # Generate predictions for all images in the test set
     mask_predictions = model.predict(test_images, verbose=2)
 
@@ -38,7 +40,7 @@ def predict_func():
         plt.subplot(3, 1, 1)
         test_image = load_img(test_input_img_paths[i])
         plt.imshow(test_image)
-        
+
         plt.subplot(3, 1, 2)
         # consider the most probable class for each predicted pixel (model returns the probability of every class per pixel)
         predicted_mask = np.argmax(predictions[i], axis=-1)
@@ -48,7 +50,8 @@ def predict_func():
         w, h = test_image.size
         # resize to original shape
         predicted_mask = tf.image.resize(predicted_mask, [h, w])
-        predicted_mask = tf.keras.preprocessing.image.array_to_img(predicted_mask)
+        predicted_mask = tf.keras.preprocessing.image.array_to_img(
+            predicted_mask)
         plt.imshow(predicted_mask)
 
         plt.subplot(3, 1, 3)
@@ -62,7 +65,7 @@ def predict_func():
     ###############################################################
 
     ##EVALUATE PREDICTIONS #####################################################################
-    history_dict = json.load(open(model_dir + "/history.json", "r"))
+    history_dict = json.load(open(pred_dir + "/history.json", "r"))
     # Mean IOU graph
     graph(history_dict,
           title='Mean IoU Graph',
@@ -79,6 +82,14 @@ def predict_func():
           history_name='accuracy',
           history_val_name='val_accuracy',
           save_path=pred_dir+'/accuracy_graph.png')
+    # Loss graph
+    graph(history_dict,
+          title='Loss Graph',
+          xlabel='Epochs',
+          ylabel='Loss',
+          history_name='loss',
+          history_val_name='val_loss',
+          save_path=pred_dir+'/loss_graph.png')
     # F1 Score graph per label
     for label in range(0, NUM_CLASSES):
         graph(
@@ -90,42 +101,63 @@ def predict_func():
             history_val_name="val_f1score"+str(label),
             save_path=pred_dir + "/f1_score_"+str(label)+"_graph.png",
         )
+    # Precision graph per label
+    for label in range(0, NUM_CLASSES):
+        graph(
+            history_dict,
+            title="Precision Graph For Label "+str(label),
+            xlabel="Epochs",
+            ylabel="Precision",
+            history_name="precision"+str(label),
+            history_val_name="val_precision"+str(label),
+            save_path=pred_dir + "/precision_"+str(label)+"_graph.png",
+        )
+        # Recall graph per label
+    for label in range(0, NUM_CLASSES):
+        graph(
+            history_dict,
+            title="Recall Graph For Label "+str(label),
+            xlabel="Epochs",
+            ylabel="Recall",
+            history_name="recall"+str(label),
+            history_val_name="val_recall"+str(label),
+            save_path=pred_dir + "/recall_"+str(label)+"_graph.png",
+        )
     #################################################################
 
     ##CLASSIFICATION REPORT###############################################################
     from sklearn.metrics import classification_report
-    target_names = [('class '+str(i)) for i in range(0,NUM_CLASSES)] #TODO: mettere nomi alle classi
-    y_true=test_masks
-    y_pred=np.argmax(mask_predictions, axis=-1)
+    # TODO: mettere nomi alle classi
+    target_names = [('class '+str(i)) for i in range(0, NUM_CLASSES)]
+    y_true = test_masks
+    y_pred = np.argmax(mask_predictions, axis=-1)
     # print(multilabel_confusion_matrix(y_true.flatten('C'),y_pred.flatten('C'), labels=[0,1,2])) #TODO: array labels più carino
     # print(classification_report(y_true.flatten('C'),y_pred.flatten('C')))
-    #save classification  report on a file
-    report=classification_report(y_true.flatten('C'),y_pred.flatten('C'),  output_dict=True)
-    cm=multilabel_confusion_matrix(y_true.flatten('C'),y_pred.flatten('C'), labels=[0,1,2])
-    df = pandas.DataFrame(report).transpose() #leave transpose because csv file output gets transposed
+    # save classification  report on a file
+    report = classification_report(y_true.flatten(
+        'C'), y_pred.flatten('C'),  output_dict=True)
+    cm = multilabel_confusion_matrix(y_true.flatten(
+        'C'), y_pred.flatten('C'), labels=[0, 1, 2])
+    # leave transpose because csv file output gets transposed
+    df = pandas.DataFrame(report).transpose()
     # print(df.T)
     df.to_csv(pred_dir+'/classification_report.csv')
-    index=0
+    index = 0
     for cm_i in cm:
         df_confusion = pandas.DataFrame(cm_i).transpose()
         df_confusion.to_csv(pred_dir+'/confusion_matrix'+str(index)+'.csv')
-        index=index+1
+        index = index+1
     #################################################################
-
 
     ##CONFUSION MATRIX ###############################################################
     from sklearn.metrics import ConfusionMatrixDisplay
-    ConfusionMatrixDisplay.from_predictions(y_true.flatten('C'),y_pred.flatten('C'))
-    plt.savefig(pred_dir + "/multiclass_confusion_matrix.png")
+    ConfusionMatrixDisplay.from_predictions(
+        y_true.flatten('C'), y_pred.flatten('C'))
+    cm_savepath = pred_dir + "/multiclass_confusion_matrix.png"
+    plt.savefig(cm_savepath)
+    # save .tex pictures
+    filename, file_extension = os.path.splitext(cm_savepath)
+    tikzplotlib.save(cm_savepath.replace(file_extension, '.tex'))
     # plt.show()
     ###############################################################
-
-
-    ##TRAIN REPORT###############################################################
-    
-
-    ###############################################################
-
-
-
 
