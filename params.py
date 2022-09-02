e3dec65501cce31688475106e5df382a0fdb563d
caterina leonelli <caterina.leonelli@gmail.com>
2022-09-02 21:43:09 +0200

    ADD patience ad monitor option DEL nargs='+'

--- params.py
+++ params.py
@@ -33,11 +33,21 @@ if args and args[0].startswith("-TP"):
                         version='%(prog)s 1.0'
                         )
     parser.add_argument("-e", "--epochs",
-                        nargs='+',
                         required=False,
                         help="number of epochs to train a model",
                         type=int,
                         default=3,
+                        )
+    parser.add_argument("-pat", "--patience",
+                        required=False,
+                        help="number of patience for earlystopping callback during fit",
+                        type=int,
+                        default=50,
+                        )
+    parser.add_argument("-mon", "--monitor",
+                        required=False,
+                        help="Metric to monitor for earlystopping and modelcheckpoint callbacks during fit",
+                        default='val_loss',
                         )
     parser.add_argument("-c", "--comment",
                         nargs='?',
@@ -46,23 +56,21 @@ if args and args[0].startswith("-TP"):
                         default="no comment",
                         )
     parser.add_argument("-d", "--dir-dataset",
-                        nargs='+',
                         required=True,
                         help="absolute directory path where dataset is stored",
                         )
     parser.add_argument("-m", "--model-name",
-                        nargs='+',
                         required=True,
                         choices=['UNET', 'TRANSFER_LEARNING_VGG16',
                                  'TRANSFER_LEARNING_VGG19'],
                         help="name of the model to train",
                         )
     parser.add_argument("-s", "--save-path",
-                        nargs='+',
                         required=False,
-                        default=parser.parse_args(args=args).dir_dataset[0],
+                        default=parser.parse_args(args=args).dir_dataset,
                         help="absolute directory path where store results",
                         )
+
 elif args and args[0].startswith("-P"):
     # PROG: ONLY PREDICT
     parser = argparse.ArgumentParser(prog='P', usage='%(prog)s [options]')
@@ -81,7 +89,6 @@ elif args and args[0].startswith("-P"):
                         version='%(prog)s 1.0'
                         )
     parser.add_argument("-mw", "--model-weights",
-                        nargs='+',
                         required=True,
                         type=argparse.FileType('r'),
                         help="absolute file path where weights of a model are saved",
@@ -93,23 +100,20 @@ elif args and args[0].startswith("-P"):
                         default="test raw",
                         )
     parser.add_argument("-m", "--model-name",
-                        nargs='+',
                         required=True,
                         choices=['UNET', 'TRANSFER_LEARNING_VGG16',
                                  'TRANSFER_LEARNING_VGG19'],
                         help="name of the model to load",
                         )
     parser.add_argument("-d", "--dir-dataset",
-                        nargs='+',
                         required=True,
                         type=dir_path,
                         help="absolute directory path where dataset is stored",
                         )
     parser.add_argument("-s", "--save-path",
-                        nargs='+',
                         required=False,
                         type=dir_path,
-                        default=parser.parse_args(args=args).dir_dataset[0],
+                        default=parser.parse_args(args=args).dir_dataset,
                         help="absolute directory path where store results",
                         )
 args = parser.parse_args(args=args)
@@ -125,21 +129,25 @@ IMAGE_SIZE = (SIZE_X, SIZE_Y)
 NUM_CLASSES = 3
 BATCH_SIZE = 8
 EPOCHS = 0
+patience = 50
+monitor= 'val_loss'
 pred_dir = args.save_path+"/predictions" + \
     datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 if args.only_predict:
     program = 'ONLY_PREDICT'
     description = args.comment
-    base_dir = args.dir_dataset[0]
-    weights_path = args.model_weights[0].name
-    MODEL_NAME = args.model_name[0]
-if args.train_and_predict :
+    base_dir = args.dir_dataset
+    weights_path = args.model_weights.name
+    MODEL_NAME = args.model_name
+if args.train_and_predict:
     program = 'TRAIN_AND_PREDICT'
     description = args.comment
-    base_dir = args.dir_dataset[0]
-    MODEL_NAME = args.model_name[0]
-    EPOCHS = args.epochs[0]
+    base_dir = args.dir_dataset
+    MODEL_NAME = args.model_name
+    EPOCHS = args.epochs
+    patience = args.patience
+    monitor=args.monitor
 
 model_dir = base_dir+"model"
 train_dir = base_dir+"train"
